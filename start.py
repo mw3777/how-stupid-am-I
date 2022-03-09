bd7c9e20652f1368643343b02e780e36afa1556e
mw3777 <96492912+mw3777@users.noreply.github.com>
2022-03-09 21:04:40 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,32 +1,38 @@
 print ("hello :)")
 
-print ("""  
- __       __
-/   \    /    \
-|  . |   |  . |
- \__/     \__/
-""")
+
 
 name = input("What's your name?\n")
 
-print("Hello " + name + ", you got hacked\n\n")
+print("Hello " + name + ", whrite the name of the Person wich you wanna to look up up\n\n")
       
       
-unhackoptions = "1. Bitcoin \n2. Monero \n"
+options = "1. stupidness \n2. intelligence \n"
 
 pick = "pick 1 or 2"
 
-print("here are the unhack options: \n" + unhackoptions)
+print("here are the  options: \n" + options)
 
 order = input("Pick 1 or 2:\n")
-#you are a genius
-#et geet nach emmer nt
-#daach
-#nee order gett just enkeier geprint
+
 
 if order == "1" or order == "2":
-  print("This payment method is not available yet.")
+  print("Whrite the name of the person")
+
+
+
 else:
   #print("you got hacked" * 100)
   print("""Are you stupid??? 
   Please type either 1 or 2""")
+
+name = input("")
+
+if name == "matti" or name == "Matti": print ("there isn't any stupidness in Matti")
+
+else: print("")
+
+
+if name == "Iannis" or name == "iannis": print("this is the stupidest person in earth")
+
+else: print("")
