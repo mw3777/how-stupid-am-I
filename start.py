e904a3df1dc52ee5a09e891e2532348f9d8d588b
mw3777 <96492912+mw3777@users.noreply.github.com>
2022-03-09 20:49:50 +0100

    start.py

---
+++ start.py
@@ -0,0 +1,32 @@
+print ("hello :)")
+
+print ("""  
+ __       __
+/   \    /    \
+|  . |   |  . |
+ \__/     \__/
+""")
+
+name = input("What's your name?\n")
+
+print("Hello " + name + ", you got hacked\n\n")
+      
+      
+unhackoptions = "1. Bitcoin \n2. Monero \n"
+
+pick = "pick 1 or 2"
+
+print("here are the unhack options: \n" + unhackoptions)
+
+order = input("Pick 1 or 2:\n")
+#you are a genius
+#et geet nach emmer nt
+#daach
+#nee order gett just enkeier geprint
+
+if order == "1" or order == "2":
+  print("This payment method is not available yet.")
+else:
+  #print("you got hacked" * 100)
+  print("""Are you stupid??? 
+  Please type either 1 or 2""")
