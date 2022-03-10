0ee3300f69c7a1d80813221acf7e37ba92a52791
mw3777 <96492912+mw3777@users.noreply.github.com>
2022-03-10 12:20:04 +0100

    Update start.py

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ print ("hello :)")
 
 name1 = input("What's your name?\n")
 
-print("Hello " + name1 + ", whrite the name of the Person wich you wanna to look up up\n\n")
+print("\nHello " + name1 + ", write the name of the Person wich you wanna to look up up\n\n")
       
       
 options = "1. stupidness \n2. intelligence \n"
@@ -12,17 +12,27 @@ options = "1. stupidness \n2. intelligence \n"
 pick = "pick 1 or 2"
 
 
-print("here are the  options: \n" + options)
+print("Here are the  options: \n" + options)
 
 order = input("Pick 1 or 2:\n")
 
-if order == "2": print("not avaible yet")
-if order == "1":
+
+
+if order == "2": print("2 isn't avaible"); order2 = input("pick 1\n")
+else: print("")
+
+
+if order == "1" or order2 == "1":
   print("\n\nWhrite the name of the person")
+else: print("")
+  #need help pls
+
+
 
 name = input("")
 
 import time
+time.sleep(0.5)
 print("Wait we're processing")
 
 time.sleep(1)
@@ -34,21 +44,20 @@ print("1")
 time.sleep (1)
 
 if name == "matti" or name == "Matti": print ("there isn't any stupidness in Matti")
-
 else: print("")
 
-
 if name == "Iannis" or name == "iannis": print("this is the stupidest person in earth")
-
 else: print("")
 
 if name == "Iannis" or name == "Iannis" or name == "Matti" or name == "matti": print("Thanks using my pogramm")
 
 else:
   import random
-random = random.randint(1,5)
+  random = random.randint(1,5)
 
 if random == 1 or random == 2: print (name + " is the stupidest man on earth")
 if random == 3 or random == 4: print (name + " is not so stupid")
 if random == 5: print (name + " isn't stupid at all")
-else: print:("Are you stupid " + name1)
+
+print("\nThanks using my pogramm")
+print(":)")
