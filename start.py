754683729a126ced8a0ba51d69cb65ddb892f365
mw3777 <96492912+mw3777@users.noreply.github.com>
2022-03-10 10:37:50 +0100

    Update start.py

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ print ("hello :)")
 
 
 
-name = input("What's your name?\n")
+name1 = input("What's your name?\n")
 
 print("Hello " + name + ", whrite the name of the Person wich you wanna to look up up\n\n")
       
@@ -16,10 +16,10 @@ print("here are the  options: \n" + options)
 order = input("Pick 1 or 2:\n")
 
 
-if order == "1" or order == "2":
+if order == "1":
   print("Whrite the name of the person")
 
-
+if order == "2": print("not avaible yet")
 
 else:
   #print("you got hacked" * 100)
@@ -36,3 +36,15 @@ else: print("")
 if name == "Iannis" or name == "iannis": print("this is the stupidest person in earth")
 
 else: print("")
+
+if name == "Iannis" or name == "Iannis" or name == "Matti" or name == "matti": print("Thanks using my pogramm")
+
+else:
+import random
+random = random.randint(1,5)
+print(random) 
+
+if random == 1 or random == 2: print (name + " is the stupidest man on earth")
+if random == 3 or random == 4: print (name + " is not so stupid")
+if random == 5: print (name + " isn't stupid at all")
+else: print:("Are you stupid " + name1)
